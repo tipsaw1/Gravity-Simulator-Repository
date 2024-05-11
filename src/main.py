ac38375650bf7d17bc1b4ae8b2434acf137d3250
tipsaw1 <corsairbravo3@gmail.com>
2024-05-11 19:28:26 -0400

    added graphics; added fonts; added snail, ground, sky, and player surfaces; added player and snail rectangles; added player and snail movement

--- src/main.py
+++ src/main.py
@@ -1,32 +1,53 @@
-import pygame, math
+import pygame
 from sys import exit
 
 # Initiate pygame
 pygame.init()
 
 # Create screen
-screen_width = 600
-screen_height = 600
+screen_width = 800
+screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Gravity')
 
-#Create pixels
-pixel = pygame.Surface((100,100))
-pixel.fill('red')
+# Extra variables
+text_font = pygame.font.Font('fonts/Argentina free promo.ttf', 50)
+
+# Create surfaces
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = text_font.render('My Game', True, 'black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(bottomleft = (600, 300))
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png')
+player_rect = player_surface.get_rect(bottomleft = (80,300))
 
 # Frame rate
 clock = pygame.time.Clock()
 
 # Game loop
 while True:
-    screen.blit(pixel, (50,50))
-
     # Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+    # Draw surfaces
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
+
+    player_rect.x += 1
+
+    snail_rect.x -= 4
+    if snail_rect.right < 0: snail_rect.left = screen_width
+
+
     # Update screen
     pygame.display.update()
-    clock.tick(240)
+    clock.tick(60)
